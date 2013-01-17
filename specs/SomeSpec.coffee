cfee8ca5043ddd353d8c8e2fe4bc6fed7fd2e8b9
patricksimpson <izerop@gmail.com>
2013-01-17 16:10:00 -0500

    added missing files

---
+++ specs/SomeSpec.coffee
@@ -0,0 +1,10 @@
+describe "This App", ->
+
+  beforeEach ->
+    #do something
+
+  afterEach ->
+    #do something
+
+  it "should find the app files", ->
+    expect( typeof APP ).toEqual( "object" );
---
+++ specs/compiled/specs/SomeSpec.js
@@ -0,0 +1,11 @@
+(function() {
+
+  describe("This App", function() {
+    beforeEach(function() {});
+    afterEach(function() {});
+    return it("should find the app files", function() {
+      return expect(typeof APP).toEqual("object");
+    });
+  });
+
+}).call(this);
